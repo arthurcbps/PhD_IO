b32f5b032768f6b02989808f7d10a2b099cc1dd9
arthurcbps <42788628+arthurcbps@users.noreply.github.com>
2022-11-09 17:10:33 -0500

    fixed Jensen problem

--- pset2/sim_dgp.m
+++ pset2/sim_dgp.m
@@ -16,15 +16,15 @@ u = normrnd(0, 0.2, 1000, 10);
 % capital and labor are determined one period in advance
 
 L = zeros(1000, 10);
-L(:, 1) = (0.108)^3*(0.73)^0.8*exp(4.75 + 3.6*omega_0);
-L(:, 2:10) = (0.108)^3*(0.73)^0.8*exp(4.75 + 3.6*omega(:, 1:9));
+L(:, 1) = (0.108)^3*(0.73)^0.8*exp(6.57 + 3.6*omega_0);
+L(:, 2:10) = (0.108)^3*(0.73)^0.8*exp(6.57 + 3.6*omega(:, 1:9));
 
 K = 0.73*L;
 
 % materials are determined on the same period, and can be conditioned on
 % epsilon
 
-M = (0.4)^(5/3)*(exp(0.1 + 0.3*epsilon + omega).*L.^(0.3).*K.^(0.2)).^(4/3);
+M = (0.4)^(5/3)*exp(16/15)*(exp(0.3*epsilon + omega).*L.^(0.3).*K.^(0.2)).^(4/3);
 
 % Realized revenue depend on the actual realizations of u
 
